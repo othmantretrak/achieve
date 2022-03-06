018f0ba4b5be999a87012db9101e59df5884705a
tretrak <3trak10@gmail.com>
2022-03-06 22:25:06 +0100

    adding logo5

--- pages/thankyou.jsx
+++ pages/thankyou.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Banner2 from "../element/banner2";
+import Footer from "../layout/footer";
+import Header from "../layout/header";
 
 function thankyou() {
-  return <div>thank you for contacting us</div>;
+  return (
+    <>
+      <Header />
+      <div className="page-content bg-gray pricing-tablee">
+        {/* <!-- Banner  --> */}
+        <Banner2 name="Tarieven" />
+        {/* <!-- Banner End --> */}
+        <h2>thank you for contacting us</h2>
+      </div>
+      <Footer sitInfo={sitInfo} />
+    </>
+  );
 }
 
 export default thankyou;
